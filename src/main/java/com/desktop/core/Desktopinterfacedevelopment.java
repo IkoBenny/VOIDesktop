33691e1f51a752c2063b89901b13437a35ffacc1
Benny Iko <benny.iko.it@gmail.com>
2023-06-13 18:08:54 -0400

    On branch master  Initial commit

---
+++ src/main/java/com/desktop/core/Desktopinterfacedevelopment.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.desktop.core;
+
+/**
+ *
+ * @author benny
+ */
+public class Desktopinterfacedevelopment {
+
+    public static void main(String[] args) {
+        new LandingPage().setVisible(true);
+    }
+}
